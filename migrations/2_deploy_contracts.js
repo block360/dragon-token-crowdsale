9eeba612b2d28d5eecf0063410c843766806e689
furusiyya <billal.ariif@gmail.com>
2018-03-07 07:18:33 +0500

    one user can have multiple lockups

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,10 @@
+var dragonToken = artifacts.require("drgToken");
+var crowdsale = artifacts.require("crowdsale");
+
+module.exports = function(deployer) {
+
+  	// Deploy dragonToken, then deploy crowdsale, passing in dragonToken newly deployed address
+	
+};
+
+
